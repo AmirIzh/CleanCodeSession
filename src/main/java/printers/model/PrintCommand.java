08aac896847846cd760fdc878d4ac8091f675c1a
AmirIzhN <amir.izhaky@nice.com>
2023-04-15 17:45:59 +0300

    Quick fix 2 - Add Low budget print option: Retry 1: Without color Retry 2: Smaller text size by half Retry 3: Smaller text size by half (again)

--- src/main/java/printers/model/PrintCommand.java
+++ src/main/java/printers/model/PrintCommand.java
@@ -2,17 +2,20 @@ package printers.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.awt.*;
 import java.util.UUID;
 
 @Builder
 @Getter
+@Setter
 public class PrintCommand {
 	// Command
 	private final UUID id = UUID.randomUUID();
 
 	// General
+	private boolean lowBudgetOption;
 	private int copies;
 	private long urgencyInSeconds;
 	private double maxCost;
--- src/main/java/printers/printers/BasePrinter.java
+++ src/main/java/printers/printers/BasePrinter.java
@@ -9,6 +9,8 @@ import printers.model.PrintReport;
 import printers.model.PrinterType;
 import printers.support.Utils;
 
+import java.awt.*;
+
 @AllArgsConstructor
 public abstract class BasePrinter implements Printer {
     private PrinterType printerType;
@@ -23,6 +25,31 @@ public abstract class BasePrinter implements Printer {
             if (printTime * costPerSecond <= printCommand.getMaxCost()) {
                 Utils.print(printerType, printCommand);
                 return new PrintReport(printerType, printTime, printTime * costPerSecond);
+            }
+            else if (printCommand.isLowBudgetOption()) {
+                printCommand.setTextColor(Color.BLACK);
+                printCommand.setPaperBackgroundColor(Color.WHITE);
+                if (printTime * costPerSecond <= printCommand.getMaxCost()) {
+                    Utils.print(printerType, printCommand);
+                    return new PrintReport(printerType, printTime, printTime * costPerSecond);
+                }
+                else {
+                    printCommand.setTextSize(printCommand.getTextSize() / 2);
+                    if (printTime * costPerSecond <= printCommand.getMaxCost()) {
+                        Utils.print(printerType, printCommand);
+                        return new PrintReport(printerType, printTime, printTime * costPerSecond);
+                    }
+                    else {
+                        printCommand.setTextSize(printCommand.getTextSize() / 2);
+                        if (printTime * costPerSecond <= printCommand.getMaxCost()) {
+                            Utils.print(printerType, printCommand);
+                            return new PrintReport(printerType, printTime, printTime * costPerSecond);
+                        }
+                        else {
+                            throw new PrintTooExpensiveException(printerType, printCommand.getId(), printCommand.getMaxCost(), printTime * costPerSecond);
+                        }
+                    }
+                }
             }
             else {
                 throw new PrintTooExpensiveException(printerType, printCommand.getId(), printCommand.getMaxCost(), printTime * costPerSecond);
